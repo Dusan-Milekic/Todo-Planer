5a93ed2c17555064be502243c6242754c784fc52
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-07-13 16:47:10 +0200

    Added (un)complete task and added sounds

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 let addTaskButton = document.getElementById("add");
 let removeTaskButton = document.getElementById("remove");
+let completeTaskButton = document.getElementById("complete");
+
+let mySound = new Audio('assets/answer-correct.mp3'); // Path to your sound file
+let popSound = new Audio('assets/pop.mp3'); // Path to your sound file
+let deleteSound = new Audio('assets/error.mp3'); // Path to your sound file
+let uncompleteSound = new Audio('assets/gtasa.mp3'); // Path to your sound file
+
 order = 1;
 
 
@@ -25,6 +32,8 @@ addTaskButton.addEventListener("click", function() {
         alert("Please fill in both fields.");
     }
 
+    popSound.play(); 
+
     addedCards = document.querySelectorAll(".card");
 });
 
@@ -35,6 +44,36 @@ removeTaskButton.addEventListener("click", function() {
         if (addedCardsJS[i].changed === true) {
             allCards[i].remove();
             deleteItemFromLocalStorage(addedCardsJS[i].id);
+            deleteSound.play(); // Play sound when task is removed
+        }
+    }
+});
+
+
+completeTaskButton.addEventListener("click", function() {
+    event.preventDefault();
+    let allCards = document.querySelectorAll(".card");
+    for (let i = 0; i < addedCardsJS.length; i++) {
+        console.log(allCards[i].style.textDecoration);
+        if (addedCardsJS[i].changed === true && allCards[i].style.textDecoration !== "line-through 4px") {
+        
+            allCards[i].style.textDecoration = "line-through"; // Strikethrough effect
+            allCards[i].style.textDecorationThickness =  "4px";
+            
+            allCards[i].style.backgroundColor = "white"; // Reset background color
+            allCards[i].querySelector("h2").style.color = "#212121"; // Reset text color
+            allCards[i].style.outlineColor = "#212121"; // Reset outline color
+            mySound.play(); // Play sound when task is completed
+            addedCardsJS[i].changed = false; // Reset the changed state
+        }
+        else if (addedCardsJS[i].changed === true && allCards[i].style.textDecoration === "line-through 4px") {
+            allCards[i].style.textDecoration = "none"; // Remove strikethrough effect
+            allCards[i].style.textDecorationThickness =  "0px";
+            allCards[i].style.backgroundColor = "white"; // Reset background color
+            allCards[i].querySelector("h2").style.color = "#212121"; // Reset text color
+            allCards[i].style.outlineColor = "#212121"; // Reset outline color
+            addedCardsJS[i].changed = false; // Reset the changed state
+            uncompleteSound.play(); // Play sound when task is uncompleted
         }
     }
 });
